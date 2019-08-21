002d48d81de94748e3352b9767024e878cb82dc5
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-08-20 21:03:40 -0400

    adotions stuff

---
+++ BackgroundProcessing/ModuleBackgroundProcessing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BackgroundProcessing
+{
+    public class ModuleBackgroundProcessing : PartModule
+    {
+        [KSPField(isPersistant = true)]
+        public string module = "";
+    }
+}
